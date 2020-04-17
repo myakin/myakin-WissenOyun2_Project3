07517b8bba42604b5a9187ebafbb5c0ba901272a
Murat Yakin <yakin_murat@yahoo.com>
2020-04-17 21:28:55 +0300

    texturelar, camera follow, player controller

---
+++ Assets/0-Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject player;
+    public float followDistance=0.5f;
+    public float offsetUp=1f;
+    public float lookUpDownOffset;
+
+    void Update() {
+        transform.position = player.transform.position + (-player.transform.forward * followDistance) + (player.transform.up * offsetUp);
+        transform.rotation = player.transform.rotation * Quaternion.Euler(lookUpDownOffset, 0, 0);
+    }
+
+    
+}
---
+++ Assets/0-Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // w a s d -> w:ileri, s:geri, a:sola yurume, d:saga yurume
+    // mouse yatay kaydirma: saga sola bakma, mouse dikey kaydirma: yukari asagi bakma
+    // f: secme, toplama, ya da kapi acma, ... gibi interactionlar
+
+    public float moveSpeed = 1f;
+    public float lookSpped = 1f;
+
+    private void Update() {
+        float ver = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        if (ver>0 || ver<0) {
+            transform.position += transform.forward * ver * moveSpeed;
+        }
+        if (hor>0 || hor<0) {
+            transform.position += transform.right * hor * moveSpeed;
+        }
+        
+
+        float leftRight = Input.GetAxis("Mouse X");
+        float upDown = Input.GetAxis("Mouse Y");
+
+        if (leftRight>0 || leftRight<0) {
+            transform.rotation *= Quaternion.Euler(0, leftRight, 0);  
+        }
+        if (upDown>0 || upDown<0) {
+            Camera.main.transform.GetComponent<CameraFollow>().lookUpDownOffset += -upDown;
+        }
+
+    }
+
+}
